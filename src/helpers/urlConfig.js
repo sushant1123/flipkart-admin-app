aaaa0cb11f98125bfa589cb417ea528c357536ef
Sushant Bahirat <sushantbahirat40@gmail.com>
2022-02-18 16:41:19 +0530

    applied fix to baseUrl var in urlConfig file

--- src/helpers/urlConfig.js
+++ src/helpers/urlConfig.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://flipkart-clone-admin-app.herokuapp.com";
+//baseurl should be of the rest server and not our front-end
+const baseUrl = "https://flipkart-rest-backend-server.herokuapp.com/";
 
 export const API = `${baseUrl}/api`;
 
