efe52cee5c6b2627bc5b8fd2f64dc4cec07a2500
Sushant Bahirat <sushantbahirat40@gmail.com>
2022-02-18 16:44:52 +0530

    change baseUrl var in urlConfig file

--- src/helpers/urlConfig.js
+++ src/helpers/urlConfig.js
@@ -1,5 +1,5 @@
 //baseurl should be of the rest server and not our front-end
-const baseUrl = "https://flipkart-rest-backend-server.herokuapp.com/";
+const baseUrl = "https://flipkart-rest-backend-server.herokuapp.com";
 
 export const API = `${baseUrl}/api`;
 
